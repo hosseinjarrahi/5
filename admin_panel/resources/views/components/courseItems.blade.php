ba8b929fc32a8d6706d035f514373b5498dacdf8
hossein jarrahi <37629053+hosseinjarrahi@users.noreply.github.com>
2020-05-23 03:55:04 +0430

    fix courses

--- admin_panel/resources/views/components/courseItems.blade.php
+++ admin_panel/resources/views/components/courseItems.blade.php
@@ -1,18 +1,10 @@
 <div class="w-100" id="courseItemsParent">
 
-    <div class="btn btn-primary btn-sm my-3" onclick="generate()"><span class="fas fa-plus"></span></div>
+    <div class="btn btn-primary btn-sm my-3" onclick="generate()"><span class="fas fa-plus"></span> <span>افزودن آیتم</span></div>
 </div>
 
-<script>
-
-    let counter = 0;
-
-
-    function generate() {
-        let random = Math.random(10);
-
-        let item = `
-        <span>
+<div class="d-none">
+    <span id="courseTemplate">
         <div class="form-group">
             <input name="courseTitle[]" class="form-control" placeholder="عنوان">
         </div>
@@ -32,37 +24,49 @@
             </div>
         </div>
 
-        <div class="form-row">
-            <div class="col-11">
+        <div class="form-row align-items-center">
+            <div class="col-10">
                 <div class="form-group my-3">
                     <div class="custom-control custom-checkbox my-1 mr-sm-2">
                         <input type="checkbox"
                                name="free[]"
                                class="custom-control-input"
-                               id="free-${random}"
+                               id="free-"
                         >
-                        <label class="custom-control-label" for="free-${random}">
+                        <label class="custom-control-label" for="free-">
                             رایگان
                         </label>
                     </div>
                 </div>
             </div>
 
-            <div class="col-1 d-flex align-items-start">
-                <div class="btn mr-auto btn-danger btn-sm" onclick="removeItem(this)"><span class="fas fa-times"></span></div>
+            <div class="col-2 d-flex align-items-start">
+                <div class="btn mr-auto btn-danger btn-sm" onclick="removeItem(this)">
+                    <span class="fas fa-times"></span>
+                    <span>حذف آیتم</span>
+                </div>
             </div>
         </div>
 
         <div class="dropdown-divider"></div>
         </span>
-        `;
+</div>
 
-        courseItemsParent.innerHTML += item;
+<script>
+    let template = courseTemplate;
+    let parent = courseItemsParent;
 
+    function generate() {
+        let random = Math.random(10);
 
+        let item = template.cloneNode(true);
+        item.querySelector('.custom-checkbox').childNodes[1].id = random;
+        item.querySelector('.custom-checkbox').childNodes[3].id = random;
+
+        parent.appendChild(item);
     }
 
-    function removeItem(ev){
+    function removeItem(ev) {
         let shouldRemove = ev.parentElement.parentElement.parentElement;
         shouldRemove.remove();
     }
